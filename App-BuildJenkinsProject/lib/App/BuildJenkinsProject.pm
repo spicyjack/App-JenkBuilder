021317a37aec01757a6b6e1ecb7f68a31ace207f
Brian Manning <brian@xaoc.org>
2013-05-31 16:32:38 -0700

    project: initial commit

---
+++ App-BuildJenkinsProject/lib/App/BuildJenkinsProject.pm
@@ -0,0 +1,110 @@
+package App::BuildJenkinsProject;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+App::BuildJenkinsProject - The great new App::BuildJenkinsProject!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use App::BuildJenkinsProject;
+
+    my $foo = App::BuildJenkinsProject->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Brian Manning, C<< <xaoc at cpan dot org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-app-buildjenkinsproject at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-BuildJenkinsProject>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc App::BuildJenkinsProject
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-BuildJenkinsProject>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/App-BuildJenkinsProject>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/App-BuildJenkinsProject>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/App-BuildJenkinsProject/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Brian Manning.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of App::BuildJenkinsProject
---
+++ README.md
@@ -0,0 +1,9 @@
+# App::BuildJenkinsProject #
+
+Runs Jenkins jobs specified in a configuration file in order to build an
+entire project.
+
+## Todo ##
+- Generate this README or the regular docs from POD
+
+vim: filetype=markdown shiftwidth=2 tabstop=2
